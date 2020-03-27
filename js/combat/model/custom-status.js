ed50703e98072da073696f10f9671f49f40da63a
Jonathan Sun <bakafish@berkeley.edu>
2020-03-26 20:14:36 -0700

    Remove hardcoding of combat status indices.

--- js/combat/model/custom-status.js
+++ js/combat/model/custom-status.js
@@ -1,6 +1,8 @@
 ig.module("game.feature.combat.model.custom-status").requires(
         "game.feature.combat.model.combat-status",
-        "game.feature.player.modifiers")
+        "game.feature.player.modifiers",
+        "game.feature.combat.gui.custom-status-bar",
+        "game.feature.combat.model.modifier-apply")
     .defines(function() {
         sc.MODIFIERS.TOXIC_HAZARD = {
             altSheet: "media/gui/status-gui.png",
@@ -9,7 +11,7 @@ ig.module("game.feature.combat.model.custom-status").requires(
             icon: -1,
             order: 100
         };
-        sc.PoisonStatus = sc.COMBAT_STATUS[5] = sc.CombatStatusBase.extend({
+        sc.PoisonStatus = sc.CombatStatusBase.extend({
             id: 0,
             label: "poison",
             statusBarEntry: "POISONED",
@@ -29,6 +31,7 @@ ig.module("game.feature.combat.model.custom-status").requires(
                 }
             }
         });
+        var poisonIdx = sc.COMBAT_STATUS.push(sc.PoisonStatus) - 1;
         sc.STATUS_BAR_ENTRY.POISONED = {
             icon: 0,
             gfx: "media/gui/poison-status.png",
@@ -41,7 +44,6 @@ ig.module("game.feature.combat.model.custom-status").requires(
         var aConst = 0.25,
             dConst = 1.5,
             cConst = 3;
-        !sc.DAMAGE_MODIFIER_FUNCS && (sc.DAMAGE_MODIFIER_FUNCS = {});
         sc.DAMAGE_MODIFIER_FUNCS.TOXIC_HAZARD = (attackInfo, damageFactor, combatantRoot, shieldResult, hitIgnore, params) => {
             var l = attackInfo.noHack || false,
                 r = attackInfo.attackerParams.getStat("focus", l) / params.getStat("focus", l),
@@ -51,9 +53,9 @@ ig.module("game.feature.combat.model.custom-status").requires(
                 q = attackInfo.element;
             q && (p = params.getStat("elemFactor")[q - 1] * params.tmpElemFactor[q - 1]);
             var pppm = r * attackInfo.attackerParams.getModifier("TOXIC_HAZARD") * p;
-            if (pppm > 0) pppm = params.statusStates[5].getInflictValue(pppm, params, attackInfo, shieldResult);
+            if (pppm > 0) pppm = params.statusStates[poisonIdx].getInflictValue(pppm, params, attackInfo, shieldResult);
             var applyDamageCallback = () => {
-                pppm && params.statusStates[5].inflict(pppm, params, attackInfo);
+                pppm && params.statusStates[poisonIdx].inflict(pppm, params, attackInfo);
             };
             return { attackInfo, damageFactor, applyDamageCallback }
         };
